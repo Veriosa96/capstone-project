cff82a764c0a579747b0637b828598bd79d4a45f
Veriosa96 <115984122+Veriosa96@users.noreply.github.com>
2023-05-31 12:03:13 +0200

    Add files via upload

---
+++ calendar_crossFit/src/test/java/it/sc/calendar_crossFit/CalendarCrossFitApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sc.calendar_crossFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarCrossFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
