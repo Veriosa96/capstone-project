61bfa89c771b5dc24a86a39c9f737701a9b6b6be
Veriosa96 <115984122+Veriosa96@users.noreply.github.com>
2023-05-31 21:53:42 +0200

    Delete calendar_crossFit/src/main/java/it/sc/calendar_crossFit directory

--- calendar_crossFit/src/main/java/it/sc/calendar_crossFit/auth/entity/ERole.java
+++
@@ -1,7 +0,0 @@
-package it.sc.calendar_crossFit.auth.entity;
-
-public enum ERole {
-	ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_MODERATOR;
-}
--- calendar_crossFit/src/main/java/it/sc/calendar_crossFit/auth/exception/ClassAlreadyBookedException.java
+++
@@ -1,7 +0,0 @@
-package it.sc.calendar_crossFit.exception;
-
-public class ClassAlreadyBookedException extends Exception {
-    public ClassAlreadyBookedException(String message) {
-        super(message);
-    }
-}
