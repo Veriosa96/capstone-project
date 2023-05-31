32dfef100a9e3bfb91a45b759489ae39da9796d2
Veriosa96 <115984122+Veriosa96@users.noreply.github.com>
2023-05-31 21:55:25 +0200

    Add files via upload

---
+++ calendar_crossFit/src/main/CalendarCrossFitApplication.java
@@ -0,0 +1,13 @@
+package it.sc.calendar_crossFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarCrossFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarCrossFitApplication.class, args);
+	}
+
+}
